c83a9c44a9b4408ba290273b6a954a2058c4d83c
Reuben Thomas <rrt@sc3d.org>
2018-08-09 22:14:49 +0100

    Update .dir-locals.el to fix

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,8 @@
 ;; Adapted from https://stackoverflow.com/questions/4012321/how-can-i-access-the-path-to-the-current-directory-in-an-emacs-directory-variabl
-((nil . ((eval . (set (make-local-variable 'ispell-personal-dictionary)
-                      (expand-file-name
-                       "spelling-list.txt"
-                       (file-name-directory
-                        (let ((d (dir-locals-find-file ".")))
-                          (if (stringp d) d (car d))))))))))
+((latex-mode . ((eval . (progn
+                         (set (make-local-variable 'ispell-personal-dictionary)
+                              (expand-file-name
+                               "spelling-list.txt"
+                               (file-name-directory
+                                (let ((d (dir-locals-find-file ".")))
+                                  (if (stringp d) d (car d)))))))))))
